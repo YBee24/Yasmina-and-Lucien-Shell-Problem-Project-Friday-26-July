e20bab6d0f1e41f0bff95b886ccf28890474de81
luciensday <lucienlmp@hotmail.com>
2024-07-26 09:33:00 +0000

    add colour and fix the all report option display

--- main.sh
+++ main.sh
@@ -2,16 +2,21 @@
 CPU_THRESHOLD=80
 MEMORY_THRESHOLD=80
 
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
 check_cpu_usage() {
   CPU_USAGE=$(ps -eo pcpu | awk 'NR>1' | awk '{sum+=$1} END {print sum}')
-  echo -e "CPU Usage: $CPU_USAGE%"
+  echo -e "CPU Usage: ${YELLOW}$CPU_USAGE%${NC}"
   if (( $(echo "$CPU_USAGE > $CPU_THRESHOLD" | bc -l) )); then
-    echo -e "ALERT: CPU usage is above $CPU_THRESHOLD%"
+    echo -e "${RED}ALERT: CPU usage is above $CPU_THRESHOLD%${NC}"
   else
-    echo -e "CPU usage is within normal range."
+    echo -e "${GREEN}CPU usage is within normal range.${NC}"
   fi
 }
-
 
 check_memory_usage() {
   MEMORY_USAGE=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
@@ -32,6 +37,41 @@ top_cpu_processes() {
   echo -e "${YELLOW}Top 3 CPU consuming processes:${NC}"
   ps -eo pid,ppid,cmd,%cpu --sort=-%cpu | head -n 4 | tail -n 3 | awk '{print $1 " " $2 " " $3 " " $4 "%"}'
 }
+
+
+
+
+generate_report() {
+  echo "Generating report..."
+  
+  echo -e "${YELLOW}System Monitoring Report - $(date)${NC}"
+  echo "----------------------------------"
+  check_cpu_usage
+  echo "----------------------------------"
+  top_cpu_processes
+  echo "----------------------------------"
+  check_memory_usage
+  echo "----------------------------------"
+  top_memory_processes
+  echo "----------------------------------"
+  check_disk_usage
+  echo "----------------------------------"
+  
+  
+  
+  
+  REPORT="report.txt"
+  {
+    check_cpu_usage
+    check_memory_usage
+    top_cpu_processes
+  } > $REPORT
+  echo -e "${GREEN}Report generated: $REPORT${NC}"
+}
+
+
+
+
 
 
 PS3="Please select an option: "
@@ -60,10 +100,17 @@ do
  echo -e "\nWould you like to download the report? (yes/no)"
   read download_report
   if [[ $download_report == "yes" ]]; then
-    generate_report
-    echo -e "${GREEN}Downloading the report...${NC}"
-    cp report.txt /path/to/your/desired/location/
-    echo -e "${GREEN}Report downloaded to /path/to/your/desired/location/report.txt${NC}"
+   generate_report
+  DESKTOP_DIR="$HOME/Desktop"
+      DOWNLOADS_DIR="$HOME/Downloads"
+      
+      if [ -d "$DESKTOP_DIR" ]; then
+        cp report.txt "$DESKTOP_DIR/report.txt"
+        echo -e "${GREEN}Report downloaded to $DESKTOP_DIR/report.txt${NC}"
+      else
+        cp report.txt "$DOWNLOADS_DIR/report.txt"
+        echo -e "${GREEN}Desktop directory not found. Report downloaded to $DOWNLOADS_DIR/report.txt${NC}"
+      fi
   else
     echo -e "${YELLOW}Report not downloaded.${NC}"
   fi
